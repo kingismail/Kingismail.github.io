bb88a9f2731fbdb9271b2f76564e0d9a43f0294e
Ismail <68245583+kingismail@users.noreply.github.com>
2023-05-22 11:44:05 +0200

    Add files via upload

---
+++ ITsolutions/index.js
@@ -0,0 +1,10 @@
+
+
+const nav_bar = document.getElementById('navi');
+const tittle = document.getElementById('welcome');
+
+window.addEventListener('load', function(){
+
+    nav_bar.style.marginLeft = "0vw";
+
+});
